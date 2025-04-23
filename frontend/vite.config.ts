6f3f2b867d10a9385f06b9c3c9e475bd254d0e70
Akshat Vyas <akshatmanojvyas@gmail.com>
2025-04-22 13:01:32 -1200

    package.json changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/expenseTracker/', // 👈 This line is KEY for GitHub Pages
+  build: {
+    outDir: 'build' // 👈 Match this with your deploy script or change it to 'dist'
+  }
 })
